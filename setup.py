032a7a57316c8ce32403d814f93c85ea44d4d904
Pranav-Saxena <pranavsaxena2003@gmail.com>
2024-03-23 18:40:31 +0530

    auto drone

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'px4_arming'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + 'visualize.rviz']),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
+        (os.path.join('share', package_name), glob('resource/*rviz'))
+        # (os.path.join('share', package_name), ['scripts/TerminatorScript.sh'])
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Pranav Saxena',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+                'offboard_control = px4_arming.offboard_control:main',
+                'visualizer = px4_arming.visualizer:main',
+                'waypoints = px4_arming.waypoints:main',
+                'control = px4_arming.control:main',
+                'processes = px4_arming.processes:main'
+        ],
+    },
+)
